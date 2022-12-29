871ed39468d023f611bb60bc1fdc811d468e862c
karguello90 <116777132+karguello90@users.noreply.github.com>
2022-12-29 18:48:16 -0500

    Update script.js
    writing local js file to main

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,123 +7,171 @@ let weatherAPIKey = "49b73569e66f42dc9f950608222312"
   let infowindow;
   
   function initMap() {
-    const sydney = new google.maps.LatLng(-33.867, 151.195);
-  
-    infowindow = new google.maps.InfoWindow();
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: sydney,
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -33.866, lng: 151.196 },
       zoom: 15,
     });
-  
     const request = {
-      query: "Museum of Contemporary Art Australia",
-      fields: ["name", "geometry"],
+      placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
+      fields: ["name", "formatted_address", "place_id", "geometry"],
     };
+    const infowindow = new google.maps.InfoWindow();
+    const service = new google.maps.places.PlacesService(map);
   
-    service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-        for (let i = 0; i < results.length; i++) {
-          createMarker(results[i]);
-        }
+    service.getDetails(request, (place, status) => {
+      if (
+        status === google.maps.places.PlacesServiceStatus.OK &&
+        place &&
+        place.geometry &&
+        place.geometry.location
+      ) {
+        const marker = new google.maps.Marker({
+          map,
+          position: place.geometry.location,
+        });
   
-        map.setCenter(results[0].geometry.location);
+        google.maps.event.addListener(marker, "click", () => {
+          const content = document.createElement("div");
+          const nameElement = document.createElement("h2");
+  
+          nameElement.textContent = place.name;
+          content.appendChild(nameElement);
+  
+          const placeIdElement = document.createElement("p");
+  
+          placeIdElement.textContent = place.place_id;
+          content.appendChild(placeIdElement);
+  
+          const placeAddressElement = document.createElement("p");
+  
+          placeAddressElement.textContent = place.formatted_address;
+          content.appendChild(placeAddressElement);
+          infowindow.setContent(content);
+          infowindow.open(map, marker);          
+        });
       }
     });
   }
   
-  function createMarker(place) {
-    if (!place.geometry || !place.geometry.location) return;
+  window.initMap = initMap;
   
-    const marker = new google.maps.Marker({
-      map,
-      position: place.geometry.location,
-    });
-  
-    google.maps.event.addListener(marker, "click", () => {
-      infowindow.setContent(place.name || "");
-      infowindow.open(map);
-    });
-  }
-  
-  //window.initMap = initMap;
-  initMap();
 
 //search bar for map
 function initAutocomplete() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -33.8688, lng: 151.2195 },
-      zoom: 13,
-      mapTypeId: "roadmap",
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -33.8688, lng: 151.2195 },
+    zoom: 13,
+    mapTypeId: "roadmap",
+  });
+  // Create the search box and link it to the UI element.
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  let markers = [];
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
     });
-    // Create the search box and link it to the UI element.
-    const input = document.getElementById("pac-input");
-    const searchBox = new google.maps.places.SearchBox(input);
-  
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener("bounds_changed", () => {
-      searchBox.setBounds(map.getBounds());
-    });
-  
-    let markers = [];
-  
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener("places_changed", () => {
-      const places = searchBox.getPlaces();
-  
-      if (places.length == 0) {
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
         return;
       }
-  
-      // Clear out the old markers.
-      markers.forEach((marker) => {
-        marker.setMap(null);
-      });
-      markers = [];
-  
-      // For each place, get the icon, name and location.
-      const bounds = new google.maps.LatLngBounds();
-  
-      places.forEach((place) => {
-        if (!place.geometry || !place.geometry.location) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-  
-        const icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25),
-        };
-  
-        // Create a marker for each place.
-        markers.push(
-          new google.maps.Marker({
-            map,
-            icon,
-            title: place.name,
-            position: place.geometry.location,
-          })
-        );
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
-  }
-  
-  //window.initAutocomplete = initAutocomplete;
-  initAutocomplete();
 
-   //Search button for three day weather forecast
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+//window.initAutocomplete = initAutocomplete;
+initAutocomplete();
+
+//var tag = document.createElement('script');
+
+      // tag.src = "https://www.youtube.com/iframe_api";
+      // var firstScriptTag = document.getElementsByTagName('script')[0];
+      // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // // 3. This function creates an <iframe> (and YouTube player)
+      // //    after the API code downloads.
+      // var player;
+      // function onYouTubeIframeAPIReady() {
+      //   player = new YT.Player('player', {
+      //     height: '390',
+      //     width: '640',
+      //     videoId: 'Kc-3B6CWHTo',
+      //     playerVars: {
+      //       'playsinline': 1
+      //     },
+      //     events: {
+      //       'onReady': onPlayerReady,
+      //       'onStateChange': onPlayerStateChange
+      //     }
+      //   });
+      // }
+
+      // // 4. The API will call this function when the video player is ready.
+      // function onPlayerReady(event) {
+      //   event.target.playVideo();
+      // }
+
+      // // 5. The API calls this function when the player's state changes.
+      // var done = false;
+      // function onPlayerStateChange(event) {
+      //   if (event.data == YT.PlayerState.PLAYING && !done) {
+      //     setTimeout(stopVideo, 6000);
+      //     done = true;
+      //   }
+      // }
+      // function stopVideo() {
+      //   player.stopVideo();
+      // }
+
+    //Search button for three day weather forecast
     $("#search-button").on("click",function(){
       let searchValue = $("#search-input").val()
       console.log(searchValue)
@@ -151,5 +199,4 @@ function initAutocomplete() {
         }
       })
      }
-
 
